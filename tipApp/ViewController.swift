ffed949de524e64d08e241bcbe9fab84514c087b
Justin Peng <j.tim.peng@gmail.com>
2015-12-27 09:31:35 -0800

    Initial Commit

---
+++ tipApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipApp
+//
+//  Created by Justin Peng on 12/27/15.
+//  Copyright © 2015 Justin Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
