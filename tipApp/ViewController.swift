0a7e5079951138d67c14144e3e3984c7b9bcd45a
highperch <jpeng@mba-001646.local>
2016-01-02 20:22:42 -0800

    Basic hide animation and text styling
    Non-input fields are hidden at load. Editing the Bill value will expose
    the tip options and total.
    
    Added some styling to the total amount to make it easier to see.

--- tipApp/ViewController.swift
+++ tipApp/ViewController.swift
@@ -12,14 +12,24 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipCaption: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalCaption: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var Divider: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$10.00"
+        Divider.alpha = 0
+        tipCaption.alpha = 0
+        tipLabel.alpha = 0
+        totalCaption.alpha = 0
+        totalLabel.alpha = 0
+        tipControl.alpha = 0
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,6 +50,17 @@ class ViewController: UIViewController {
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+    }
+    @IBAction func ShowTipTotal(sender: AnyObject) {
+        UIView.animateWithDuration(0.5, animations: {
+        self.Divider.alpha = 1
+        self.tipControl.alpha = 1
+        self.tipCaption.alpha = 1
+        self.tipLabel.alpha = 1
+        self.totalCaption.alpha = 1
+        self.totalLabel.alpha = 1
+        })
+
     }
 
     @IBAction func onTap(sender: AnyObject) {
